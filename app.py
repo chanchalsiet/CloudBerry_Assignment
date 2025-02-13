88d8a82b9cda76907381ff2dee648779dd85506b
Chanchal <137641438+chanchalsiet@users.noreply.github.com>
2025-02-14 03:51:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+
+#Create a new Flask page with a form that asks for a user’s name and displays a greeting
+from flask import Flask, redirect, url_for, request,render_template
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+@app.route('/login', methods=['GET'])
+def login():
+    if request.method == 'GET':
+        user = request.args.get('fname')
+        return 'Welcome %s' % user
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
+
+#https://github.com/chanchalsiet/CloudBerry_Assignment/tree/main
+
